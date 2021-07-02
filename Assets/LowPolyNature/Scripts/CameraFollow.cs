18fee9e02507e0db0b1cd188e9f06b9c16078c24
Mo_Wizard <oacooke04@gmail.com>
2021-07-02 12:28:14 +1000

    Completed the Camera Controller
    The Camera works with both Mouse and Joystick!
    This may need future testing once the player model has been completed.

--- Assets/LowPolyNature/Scripts/CameraFollow.cs
+++ Assets/LowPolyNature/Scripts/CameraFollow.cs
@@ -11,21 +11,75 @@ public class CameraFollow : MonoBehaviour
     public float inputSensitivity = 150.0f;
     public GameObject CameraObject;
     public GameObject PlayerObject;
+
+    // Camera distance from the player
     public float camDistanceXToPlayer;
     public float camDistanceYToPlayer;
     public float camDistanceZToPlayer;
+
+    // Mouse position
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+
+    public float smoothX;
+    public float smoothY;
+
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
 
     //https://youtu.be/LbDQHv9z-F0 6:40
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Set up the rotation of the joysticks
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+
+        // Get the input from the mouse
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        // Combine the mouse and joystick input together
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        // Rotate according to the final input and the sensitivity
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+        // Clamps the angle so it can't go above or below certain angles
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        // Set target to follow
+        Transform target = CameraFollowObject.transform;
+
+        // Move towards the target
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
     }
 }
